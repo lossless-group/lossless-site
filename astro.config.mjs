b9408c0d1d2f236900f729bf3d50adb340321e9c
mpstaton <mpstaton@gmail.com>
2025-05-05 21:56:26 +0300

    milestone(toc): Table of Contents now renders correctly, and is its own component
     On branch development
     Your branch is up to date with 'origin/development'.
     Changes to be committed:
    	modified:   astro.config.mjs
    	modified:   src/components/articles/OneArticleOnPage.astro
    	modified:   src/components/markdown/AstroMarkdown.astro
    	new file:   src/components/markdown/TableOfContents.astro
    	modified:   src/layouts/OneArticle.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import node from '@astrojs/node';
 import rehypeMermaid from 'rehype-mermaid';
 import rehypeRaw from 'rehype-raw'; // Import rehype-raw
 import normalizeShellLangs from './src/utils/markdown/normalizeShellLangs.js';
+import remarkTableOfContents from './src/utils/markdown/remark-toc';
 import vercel from '@astrojs/vercel';
 
 /** @type {ShikiLang[]} */
@@ -30,6 +31,7 @@ export default defineConfig({
     },
     remarkPlugins: [
       /** @type {any} */ (normalizeShellLangs),
+      /** @type {any} */ (remarkTableOfContents),
     ],
     remarkRehype: {
       allowDangerousHtml: true,
